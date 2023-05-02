0b38dd46716b051be9ed4006b7ed8074535275ee
kale <42823429+oelin@users.noreply.github.com>
2023-05-02 10:57:06 +0100

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,7 @@
 import World from './world.js'
 import Vector from './vector.js'
 import Matrix from './matrix.js'
-import { Cube } from './meshes.js'
+import { Prism, Cube } from './meshes.js'
 
 
 window.onload = function() {
@@ -11,13 +11,60 @@ window.onload = function() {
 
   let matRotx, matRoty
   let theta = new Vector()
+  let theta_velocity = new Vector()
+  let velocity = new Vector()
+  let position = new Vector(0, 0, 2)
+
+  window.onkeydown = function({ key }) {
+        if (key === 'ArrowLeft') {
+                theta_velocity.y = 0.1
+        }
+
+        else if (key == 'ArrowRight') {
+                theta_velocity.y = -0.1
+        }
+
+        else if (key == 'ArrowUp') {
+                theta_velocity.x = 0.1
+        }
+
+        else if (key == 'ArrowDown') {
+                theta_velocity.x = -0.1
+        }
+
+        else if (key == 'a') {
+                velocity.x = -0.1
+        }
+
+        else if (key == 'd') {
+                velocity.x = 0.1
+        }
+
+        else if (key == 'w') {
+                velocity.y = -0.1
+        }
+
+        else if (key == 's') {
+                velocity.y = 0.1
+        }
+
+        else if (key == 'e') {
+                velocity.z = 0.1
+        }
+
+        else if (key == 'q') {
+                velocity.z = -0.1
+        }
+  }
 
   world.draw = function() {
+    let cube = new Cube()
     this.clear()
 
-    let cube = new Cube()
-    //theta.x -= 0.01
-    theta.y -= 0.01
+    velocity.scale(1 / 1.1)
+    theta_velocity.scale(1 / 1.1)
+    theta.add(theta_velocity)
+    position.add(velocity)
 
     matRotx = Matrix.fromArray([
       [1.0, 0.0, 0.0],
@@ -34,6 +81,7 @@ window.onload = function() {
     cube.transform(matRotx)
     cube.transform(matRoty)
     cube.add(new Vector(0, 0, 2))
+    cube.add(position)
 
     this.paint(cube)
   }
